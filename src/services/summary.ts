3a3fbd9738982471d661e599bdfbbae37a10c685
cybrd <brysuralta@gmail.com>
2024-11-04 03:12:29 +0800

    add id to total points

--- src/services/summary.ts
+++ src/services/summary.ts
@@ -6,14 +6,14 @@ export const getMembersTotalPoints = (db: IDatabase<object>) => {
   console.log("getMembersTotalPoints");
 
   const query = `
-    SELECT discord_name, ingame_name,
+    SELECT discord_name, ingame_name, m.id as id,
     SUM(CASE WHEN a."type" IN ('Bestowed (Archboss)') THEN 0 ELSE a.points END) AS available_points,
     SUM(CASE WHEN a."type" IN ('Bestowed') THEN 0 ELSE a.points END) AS available_archboss_points,
     SUM(CASE WHEN a."type" IN ('Bestowed', 'Bestowed (Archboss)') THEN 0 ELSE a.points END) AS total_points
     FROM members m
     JOIN member_activity ma ON ma.member_id = m.id
     JOIN activity a ON a.id = ma.activity_id
-    GROUP BY 1, 2
+    GROUP BY 1, 2, 3
     ORDER BY available_points DESC
   `;
 

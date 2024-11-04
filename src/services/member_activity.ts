51a94f581a069777bca5c2a0a27343c482a9ba22
cybrd <brysuralta@gmail.com>
2024-11-04 20:40:42 +0800

    ORDER BY activity_date

--- src/services/member_activity.ts
+++ src/services/member_activity.ts
@@ -11,6 +11,7 @@ export const getMembersByActivityId = (db: IDatabase<object>, id: string) => {
     JOIN activity a ON a.id = ma.activity_id
     JOIN members m ON m.id = ma.member_id
     WHERE ma.activity_id = $1
+    ORDER BY activity_date DESC
   `;
 
   return db.manyOrNone<MemberActivityFull>(query, [id]);
@@ -25,6 +26,7 @@ export const getActivityByMemberId = (db: IDatabase<object>, id: string) => {
     JOIN activity a ON a.id = ma.activity_id
     JOIN members m ON m.id = ma.member_id
     WHERE ma.member_id = $1
+    ORDER BY activity_date DESC
   `;
 
   return db.manyOrNone<MemberActivityFull>(query, [id]);

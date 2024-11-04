58b8fe70e1c2b60c0cd8e2b3eefd07fd8e56d4cb
cybrd <brysuralta@gmail.com>
2024-11-05 01:55:58 +0800

    Update activity.sql

--- schema/activity.sql
+++ schema/activity.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS  "activity" (
   "id" VARCHAR(128) NOT NULL,
   "name" VARCHAR(128) NOT NULL,
+  "type" VARCHAR(128) NOT NULL,
   "activity_date" DATE NOT NULL,
   "points" INTEGER NOT NULL,
   "created_timestamp" TIMESTAMP NOT NULL,

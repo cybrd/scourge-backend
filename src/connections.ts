3fea05e1ce9ac704ef5ed855dc3898380a246a76
cybrd <brysuralta@gmail.com>
2024-11-07 15:38:58 +0800

    scourge schema

--- src/connections.ts
+++ src/connections.ts
@@ -3,9 +3,12 @@ import pgpClass from "pg-promise";
 import { config } from "dotenv";
 config();
 
-export const db = pgpClass()({
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-  user: "postgres",
-});
+export const db = pgpClass()(
+  {
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    user: "postgres",
+  },
+  "scourge"
+);

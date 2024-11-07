ddf76a09c82b482d1f156684fb377ce01a039284
cybrd <brysuralta@gmail.com>
2024-11-07 17:58:53 +0800

    Update connections.ts

--- src/connections.ts
+++ src/connections.ts
@@ -6,9 +6,8 @@ config();
 export const db = pgpClass({
   schema: "scourge",
 })({
-  database: "hrtx",
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   port: 5432,
-  user: "scourge",
+  user: "postgres",
 });
